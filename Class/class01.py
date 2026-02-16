14aeb31fc245afeb909f86ddd2ec225ec656dfec
gg.eduardo@proton.me <gg.eduardo@proton.me>
2026-02-16 08:01:40 -0600

    third commit

---
+++ Class/class01.py
@@ -0,0 +1,6 @@
+edad = int(input("Introduce tu edad: "))
+
+if edad < 18:
+    print("Eres menor de edad.")
+else:
+    print("Eres mayor de edad.")    
---
+++ Lab01-2/lab01_V2.py
@@ -0,0 +1,38 @@
+import utils_V2 as utils
+
+usr = input("Ingresa tu usuario: ")
+carrera = input("Ingresa tu carrera: ")
+print(f"\nBienvenido, {usr}!")
+
+while True:
+
+    utils.msg_menu()
+    opcion=str(input("Ingresa opción: "))
+ 
+    if opcion == "1":
+        a = float(input("Ingresa el primer número: "))
+        b = float(input("Ingresa el segundo número: "))
+        print(f"Suma: {utils.suma(a, b)}")
+        print(f"Resta: {utils.resta(a, b)}")
+        print(f"Multiplicación: {utils.multiplicacion(a, b)}")
+        print(f"División: {utils.division(a, b)}")
+
+    elif opcion == "2":
+        distancia = float(input("Ingresa la distancia (m): "))
+        tiempo = float(input("Ingresa el tiempo (s): "))
+        print(f"Velocidad: {utils.velocidad(distancia, tiempo)} m/s")
+
+    elif opcion == "3":
+        precio = float(input("Ingresa el precio unitario: "))
+        cantidad = int(input("Ingresa la cantidad: "))
+        print(f"Pago total: {utils.pago_total(precio, cantidad)}")
+
+    elif opcion == "4":
+        numero = float(input("Ingresa un número para verificar si es positivo: "))
+        print(utils.numeros_positivos(numero))  
+
+    elif opcion == "5":
+        nota = float(input("Ingresa una nota para verificar: "))
+        print(utils.chequeo_nota(nota))
+    else:
+        utils.salir()
